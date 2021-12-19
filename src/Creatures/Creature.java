cba6344f62f3ec2088b8ea5fb13e1c563248ea7e
fedor <fedor.prozorovV@gmail.com>
2021-12-19 15:39:29 +0300

    ez

--- src/Creatures/Creature.java
+++ src/Creatures/Creature.java
@@ -14,17 +14,6 @@ public class Creature implements Named {
     public Creature(){
     }
 
-    public Creature (String name, int hp,int dexteritySkill, int damage)
-    {
-        this.damage = damage;
-        this.name=name;
-        this.hp=100;
-        this.dexteritySkill= dexteritySkill;
-    }
-
-    public String getName(){return name;}
-    public int getDamage(){return damage;}
-
     @Override
     public String giveName() {
         String str = this.name;
